c5a728b17fc85769077ee028b552380a9f4ae082
Edgar Post-Buijs <github@edgarpost.com>
2025-12-14 15:01:34 +0100

    fix: debug spotify token

--- src/pages/auth/callback.ts
+++ src/pages/auth/callback.ts
@@ -1,43 +1,57 @@
 import type { APIRoute } from "astro";
-import { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, APP_BASE_URL } from "astro:env/server";
-import { SPOTIFY_TOKEN_COOKIE_NAME, type TokenStorage } from "../../store/auth.ts";
+import {
+  SPOTIFY_CLIENT_ID,
+  SPOTIFY_CLIENT_SECRET,
+  APP_BASE_URL,
+} from "astro:env/server";
+import {
+  SPOTIFY_TOKEN_COOKIE_NAME,
+  type TokenStorage,
+} from "../../store/auth.ts";
 
 export const GET: APIRoute = async ({ request, cookies, redirect }) => {
-  const code = new URL(request.url).searchParams.get('code');
+  const code = new URL(request.url).searchParams.get("code");
 
   if (!code) {
-    throw new Error('Code required');
+    throw new Error("Code required");
   }
 
   const searchParams = new URLSearchParams();
-  searchParams.set('code', code);
-  searchParams.set('redirect_uri', `${APP_BASE_URL}/auth/callback`);
-  searchParams.set('grant_type', 'authorization_code')
+  searchParams.set("code", code);
+  searchParams.set("redirect_uri", `${APP_BASE_URL}/auth/callback`);
+  searchParams.set("grant_type", "authorization_code");
 
   const authOptions: RequestInit = {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Authorization': 'Basic ' + (Buffer.from(SPOTIFY_CLIENT_ID + ':' + SPOTIFY_CLIENT_SECRET).toString('base64')),
-      'Content-Type': 'application/x-www-form-urlencoded'
+      Authorization:
+        "Basic " +
+        Buffer.from(SPOTIFY_CLIENT_ID + ":" + SPOTIFY_CLIENT_SECRET).toString(
+          "base64",
+        ),
+      "Content-Type": "application/x-www-form-urlencoded",
     },
-    body: searchParams
+    body: searchParams,
   };
 
-  return fetch('https://accounts.spotify.com/api/token', authOptions)
-    .then(response => response.json())
-    .then(body => {
+  console.log({ authOptions, SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET });
+
+  return fetch("https://accounts.spotify.com/api/token", authOptions)
+    .then((response) => response.json())
+    .then((body) => {
+      console.log({ body });
       if (body.access_token) {
         cookies.set(SPOTIFY_TOKEN_COOKIE_NAME, body as TokenStorage, {
-          path: '/'
+          path: "/",
         });
 
-        return redirect('/', 302);
+        return redirect("/", 302);
       }
 
-      return new Response('Error retrieving access token')
+      return new Response("Error retrieving access token");
     })
-    .catch(error => {
-      console.log({ error })
-      return new Response('Error retrieving access token')
+    .catch((error) => {
+      console.log({ error });
+      return new Response("Error retrieving access token");
     });
-}
+};
